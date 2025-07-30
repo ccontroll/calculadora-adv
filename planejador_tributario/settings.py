cfb9801397172cad0f021224f35e9e5ff747131f
AKSI Tecnologia <aksitecnologia@gmail.com>
2025-07-30 13:43:51 -0400

    Add deg environment

--- planejador_tributario/settings.py
+++ planejador_tributario/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == '1':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "calculadora-adv.c-controll.com.br"]
 
